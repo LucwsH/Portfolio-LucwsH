b5b5483a00becb65504f143dbbdd22b73c32428a
Lucas Almeida <77587741+LucwsH@users.noreply.github.com>
2023-10-16 01:32:40 -0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,6 @@
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+});
 window.addEventListener("keydown", function (e) {
   if (e.key == "F12" || (e.ctrlKey && e.shiftKey && e.key == "I") || (e.ctrlKey && e.shiftKey && e.key == "C")) {
   e.preventDefault();
